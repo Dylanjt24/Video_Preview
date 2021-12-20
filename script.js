59456c0f5c735798e3e81ad5a4a452c882db5db7
Dylan Thierbach <dylanjt15@gmail.com>
2021-12-20 13:18:08 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+// HOVER FUNCTIONALITY WORKS EVEN WITHOUT THESE FUNCTIONS, BUT THIS IS HOW THE FUNCTIONS WOULD LOOK
+
+function play(element) {
+    element.play();
+}
+
+function pause(element) {
+    element.pause();
+}
